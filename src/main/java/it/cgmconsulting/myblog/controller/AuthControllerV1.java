83b87c42982347fe7de41f0e394c59a6e0c770f6
Cavaliere <s.lorenzo@studenti.unina.it>
2024-11-29 17:22:44 +0100

    Update AuthControllerV1.java

--- src/main/java/it/cgmconsulting/myblog/controller/AuthControllerV1.java
+++ src/main/java/it/cgmconsulting/myblog/controller/AuthControllerV1.java
@@ -20,27 +20,35 @@ public class AuthControllerV1 {
     @Operation(
             summary = "ADD A NEW USER",
             description = "Method to add a new user to the database with a sign in procedure and send an email to confirm the registration",
-            tags = {"Tags"})
+            tags = {"Auth"})
     @PostMapping("/v0/auth/signup")
     public ResponseEntity<String> signup(@RequestBody @Valid SignUpRequest request){
         return ResponseEntity.ok(authService.signup(request));
     }
 
     @Operation(
-            summary = "GET VISIBLE TAGS",
-            description = "Method to get a list of all visible tags from the database",
-            tags = {"Tags"})
+            summary = "VERIFY EMAIL",
+            description = "Method to that starts from the email confirmation code and verify the email",
+            tags = {"Auth"})
     //TODO: non è un patch!
     @GetMapping("/v0/auth/confirm/{confirmCode}")
     public ResponseEntity<String> verifyEmail(@PathVariable String confirmCode){
         return ResponseEntity.ok(authService.verifyEmail(confirmCode));
     }
 
+    @Operation(
+            summary = "LOGIN",
+            description = "Method to sign in the user that exists the database to the application",
+            tags = {"Auth"})
     @PostMapping("/v0/auth/signin")
     public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody @Valid SignInRequest request){
         return ResponseEntity.ok(authService.signin(request));
     }
 
+    @Operation(
+            summary = "MODIFY USER AUTHORITY",
+            description = "Method to modify the authority of the user in the database. Only the admin can do it",
+            tags = {"Auth"})
     @PatchMapping("/v1/auth/modify_user_authority") // modify user authority
     @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<String> modifyUserAuthority(@RequestParam int id, @RequestParam String auth){
